2c5dd611013ddc069e8658c41263807cc424369c
P3nny <p3nny@hacking.studio>
2020-05-18 22:37:12 +0200

    Updates

---
+++ precache-manifest.cfe6f5d5e1541a9fc07a0d5705b1becf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee307011ad2534029e3c98aeef71759",
+    "url": "/mighty-robots/index.html"
+  },
+  {
+    "revision": "8ba6a2d983eab236a0bf",
+    "url": "/mighty-robots/static/css/main.b27a34d6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/mighty-robots/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mighty-robots/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba6a2d983eab236a0bf",
+    "url": "/mighty-robots/static/js/main.032f6978.chunk.js"
+  },
+  {
+    "revision": "95838a9a4257056fdc70",
+    "url": "/mighty-robots/static/js/runtime-main.b11e9fb6.js"
+  }
+]);
